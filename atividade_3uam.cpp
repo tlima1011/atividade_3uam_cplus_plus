a9e4c8ed32a8f7544f6903973c89acb5118196e1
Thiago <thiagolima1011@gmail.com>
2023-02-09 21:33:18 -0300

    Projeto criado com exercicio

---
+++ atividade_3uam.cpp
@@ -0,0 +1,155 @@
+#include <iostream>
+#include <string.h>
+#include <locale.h>
+
+using namespace std;
+
+typedef struct
+{
+	char nome[30];
+	int anoNascimento;
+	double montante;
+} c_cliente;
+
+int opcao = 0;
+
+/*void carregarLista()
+{
+	c_cliente cliente[10];
+	c_cliente* c;
+	c = &cliente[0];
+}*/
+
+void menu()
+{
+	cout << "+-------------------------------------------+" << endl;
+	cout << "|\t\tSISTEMA			" << endl;
+	cout << "+-------------------------------------------+" << endl;
+	cout << "| [ 1 ] - Incluir um novo cliente " << endl;
+	cout << "| [ 2 ] - Listar clientes " << endl;
+	cout << "| [ 3 ] - Remover cliente " << endl;
+	cout << "| [ 4 ] - Zerar todos montantes " << endl;
+	cout << "| [ 5 ] - Listar o melhor comprador " << endl;
+	cout << "| [ 6 ] - Exibir montante de um cliente específico " << endl;
+	cout << "| [ 7 ] - Sair " << endl;
+	cout << "+-------------------------------------------+" << endl;
+	cout << "|Opção ";
+	cin >> opcao;
+
+	/*- incluir um novo cliente
+	- remover cliente - atualizar o montante de compras do cliente realizadas no mês corrente
+	- zerar todos os montantes de compras por ocasião da virada de mês
+	- listar o cliente melhor comprador
+	- exibir um montante de compras de um cliente específico.*/
+}
+
+void incluirCliente(c_cliente* c)
+{
+	//system("cls");
+	int i = 0;
+	char cont;
+
+	do
+	{
+		cout << "Dados do " << i + 1 << "º Cliente: "  << endl;
+		cout << "Nome: ";
+		cin >> (c + i)->nome;
+		cout << "Ano Nascimento: ";
+		cin >> (c + i)-> anoNascimento;
+		cout << "Montante em Gastos  R$";
+		cin >> (c + i)->montante;
+		i++;
+		cout << "Deseja continuar [s/n]";
+		cin >> cont;
+		if((cont == 'n') || (cont == 'N'))
+		{
+			break;
+		}
+		if( i >= 10)
+		{
+			cout << "Atingido quantidade máxima de clientes" << endl; 
+			break;
+		}
+	}
+	while(i < 10 || cont == 'n' || cont == 'N');
+}
+
+void listarCliente(c_cliente* c){ 
+	for(int i = 0; i < sizeof(c_cliente) / sizeof(int); i++){
+		cout << "===================================" << endl; 
+		cout << "Dados do " << i + 1 << " cliente" << endl;
+		cout << "===================================" << endl; 
+		if((c+i)-> nome != NULL){
+			cout << "Nome: " << (c + i)->nome << endl; 
+			cout << "Ano Nascimento: " << (c+i)->anoNascimento << endl; 
+			cout << "Montante em R$" << (c+i)->montante << endl;  
+		}
+	}
+}
+
+void removerCliente(c_cliente* c){ 
+	char procurar[30];
+	//char procurar[30]; 
+	cout << "Informe um nome para remover: ";
+	cin >> procurar; 
+	for(int i = 0; i < sizeof(c_cliente) / sizeof(int); i++){
+		if(!strcmp(procurar, (c + i)-> nome)){ 
+			strcpy((c+i)->nome, "-");
+			(c+i)->anoNascimento = 0; 
+			(c+i)->montante = 0;
+		} 
+	}
+}
+
+
+void zerarMontantes(c_cliente* c){
+	char zerar; 
+	cout << "Deseja zerar montantes? [s][n]" << endl; 
+	cin >> zerar; 
+	if(zerar == 'S' || zerar == 's'){ 
+		for(int i = 0; i < sizeof(c_cliente) / sizeof(int); i++){
+			cout << "Zerando montante do " << i + 1 << " cliente: " << endl; 
+			(c+i)->montante = 0 ;
+		}
+		cout << "Montantes zerados com sucesso " << endl; 
+	}
+}
+
+
+int main(int argc, char *argv[])
+{
+
+	setlocale(LC_ALL, "Portuguese");
+	
+	int i = 0;
+	char cont;
+
+	c_cliente cliente[10];
+	c_cliente* c;
+	c = &cliente[0];
+
+	//carregarLista(&cliente);
+	do
+	{
+		menu();
+		switch(opcao)
+		{
+		case 1:
+			incluirCliente(&cliente[0]); 	
+			break;
+		case 2: 
+			listarCliente(&cliente[0]);
+			break; 
+		case 3: 
+			removerCliente(&cliente[0]); 
+			break; 
+		case 4: 
+			zerarMontantes(&cliente[0]); 
+			break; 
+		}
+
+	}while(opcao != 7);
+
+
+	return 0;
+}
